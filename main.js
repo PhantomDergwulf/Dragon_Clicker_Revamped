e1181ccfaa6a3a5196f38d79754d06c69ddfd024
CarlTheFox <carlthefox86@gmail.com>
2023-09-23 12:54:52 +0300

    Added info.txt and main.js
    It's only treated as "local" mod. Put it in your local mod folder and test it out yourself! - It'll start off as disabled though.

---
+++ main.js
@@ -0,0 +1,11 @@
+Game.registerMod("dragon revamp",{
+
+		init:function(){
+
+			Game.Notify(`Prototype dragon mod loaded! Bare bones only!`,'',[16,5]);
+		},
+		save:function(){
+		},
+		load:function(str){
+		},
+	});
